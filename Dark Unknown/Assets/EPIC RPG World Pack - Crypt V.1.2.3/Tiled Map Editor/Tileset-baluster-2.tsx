2365df517ff86219c573620ee3194c5c54c1bc8e
angelocapponcelli <angelo.capponcelli@mail.polimi.it>
2022-11-07 18:31:35 +0100

    Imported Crypt assets package

---
+++ Dark Unknown/Assets/EPIC RPG World Pack - Crypt V.1.2.3/Tiled Map Editor/Tileset-baluster-2.tsx
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Tileset-baluster-2" tilewidth="32" tileheight="32" tilecount="182" columns="13">
+ <image source="../Tilesets/Tileset-baluster-2.png" width="416" height="471"/>
+ <wangsets>
+  <wangset name="baluster-2" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="1" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="3" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="13" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="14" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="16" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="17" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="26" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="28" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="30" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="52" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="53" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="55" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="56" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="66" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="67" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="68" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
