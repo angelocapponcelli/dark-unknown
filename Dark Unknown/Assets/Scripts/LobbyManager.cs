97f0551448049979ac76f50323edd856ed96d3b4
angelocapponcelli <angelo.capponcelli@mail.polimi.it>
2022-11-08 11:21:00 +0100

    Created basic game flow
    created gameManager that keep data of the entire game run, over the scenes, created LobbyManager that manage the single scene when entering in the lobby, imported few tiles to test random generation of rooms

---
+++ Dark Unknown/Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyManager : MonoBehaviour
+{
+    [SerializeField] private GameManager _gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(_gameManager.getLobby());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
