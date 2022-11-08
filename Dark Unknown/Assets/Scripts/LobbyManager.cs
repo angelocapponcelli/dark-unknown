76be7b6c02d9d6b01e939e32b6d1276be82aaa10
angelocapponcelli <angelo.capponcelli@mail.polimi.it>
2022-11-08 11:22:36 +0100

    Update LobbyManager.cs

--- Dark Unknown/Assets/Scripts/LobbyManager.cs
+++ Dark Unknown/Assets/Scripts/LobbyManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class LobbyManager : MonoBehaviour
 {
@@ -15,9 +14,6 @@ public class LobbyManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            SceneManager.LoadScene("Game");
-        }
+        
     }
 }
