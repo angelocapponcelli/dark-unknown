ad0b3548f2794da687442b1313d6462e974a0b97
angelocapponcelli <angelo.capponcelli@mail.polimi.it>
2022-11-09 22:48:35 +0100

    Tilemap implementation

--- Dark Unknown/Assets/Scripts/CameraFollowPlayer.cs
+++ Dark Unknown/Assets/Scripts/CameraFollowPlayer.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class CameraFollowPlayer : MonoBehaviour
 {
     [SerializeField] private Transform _player;
-    [SerializeField] private float _smoothing;
+    [SerializeField] private float _smoothing = 1.0f;
     [SerializeField] private Vector3 _offset;
+    private Vector3 _playerPos;
 
-    // Update is called once per frame
-    void FixedUpdate()
+    private void Start()
     {
-        if (_player != null)
-        {
-            Vector3 newPosition = Vector3.Lerp(transform.position, _player.transform.position + _offset, _smoothing);
-            transform.position = newPosition;
-        }
+        if (_player == null) return;
+
+        _offset = transform.position - _player.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_player == null) return;
+
+        _playerPos = _player.position + _offset;
+        transform.position = Vector3.Lerp(transform.position, _playerPos, _smoothing * Time.deltaTime);
     }
 }
