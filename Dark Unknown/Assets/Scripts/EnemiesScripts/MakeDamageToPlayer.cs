e7283286a89cc841fdd58147d11b5119fba81166
angelocapponcelli <angelo.capponcelli@mail.polimi.it>
2022-11-21 16:03:48 +0100

    Spike trap Collider and UI updates

--- Dark Unknown/Assets/Scripts/EnemiesScripts/MakeDamageToPlayer.cs
+++ Dark Unknown/Assets/Scripts/EnemiesScripts/MakeDamageToPlayer.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KnifeController : MonoBehaviour
+public class MakeDamageToPlayer : MonoBehaviour
 {
-    [SerializeField] private float _damage = 20;
+    [SerializeField] private float _damage;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
