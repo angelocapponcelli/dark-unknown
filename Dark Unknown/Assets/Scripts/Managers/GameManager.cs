dad50500381c12245d017e469ef92cc980c96e00
lucafornasari <62260158+lucafornasari@users.noreply.github.com>
2022-11-22 23:11:22 +0100

    Camera Fixed

--- Dark Unknown/Assets/Scripts/Managers/GameManager.cs
+++ Dark Unknown/Assets/Scripts/Managers/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,11 +8,13 @@ public class GameManager : Singleton<GameManager>
 {
     private LevelManager _levelManager;
     [SerializeField] private Player player;
+    private CinemachineVirtualCamera _cineMachine;
     
     protected override void Awake()
     {
         base.Awake();
         _levelManager = FindObjectOfType<LevelManager>();
+        _cineMachine = FindObjectOfType<CinemachineVirtualCamera>();
     }
     
     // Start is called before the first frame update
@@ -19,6 +22,7 @@ public class GameManager : Singleton<GameManager>
     {
         _levelManager.SetInitialRoom();
         player = Instantiate(player,Vector3.zero,Quaternion.identity);
+        _cineMachine.Follow = player.transform;
     }
 
     // Update is called once per frame
