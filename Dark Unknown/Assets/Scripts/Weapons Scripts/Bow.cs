0af28d3c5edd07aeafc43c63fb5b13c8ac236281
lucafornasari <62260158+lucafornasari@users.noreply.github.com>
2022-11-24 18:21:43 +0100

    bow implemented

--- Dark Unknown/Assets/Scripts/Weapons Scripts/Bow.cs
+++ Dark Unknown/Assets/Scripts/Weapons Scripts/Bow.cs
@@ -1,21 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class Sword : Weapon
+public class Bow : Weapon
 {
-    private Animator _weaponAnimator;
-    [SerializeField] private Animator _effectAnimator;
+    //private Animator _weaponAnimator;
+    //[SerializeField] private Animator _effectAnimator;
+    [SerializeField] private GameObject _arrow;
 
     private void Start()
     {
-        _delay = 0.8f;
+        _delay = 0.2f;
         //_damage = 10f; non � pi� usato in questa classe, vedi WeaponAttack
-        _canRotateFreely = false;
+        _canRotateFreely = true;
         
-        _weaponAnimator = GetComponent<Animator>();
+        //_weaponAnimator = GetComponent<Animator>();
 
     }
 
@@ -24,9 +26,12 @@ public class Sword : Weapon
     {
         if (_attackBlocked)
             return;
-        
-        _weaponAnimator.SetTrigger("Attack");
-        _effectAnimator.SetTrigger("Attack");
+        GameObject arrow = Instantiate(_arrow, transform.position, Quaternion.Euler(0f,0f,-90f) );
+        arrow.GetComponent<Rigidbody2D>().velocity = (direction*10).normalized * 8;
+        arrow.transform.Rotate(0.0f,0.0f,rotation);
+        Destroy(arrow, 2.5f);
+
+        //_effectAnimator.SetTrigger("Attack");
         _attackBlocked = true;
         AudioManager.Instance.PlayPLayerAttackSound();
 
