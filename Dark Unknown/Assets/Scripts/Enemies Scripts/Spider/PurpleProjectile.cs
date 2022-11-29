2669436a7b6332953fe31587a0ba5aca7e73db1a
lucafornasari <62260158+lucafornasari@users.noreply.github.com>
2022-11-29 18:21:24 +0100

    arrow, spider fix

--- Dark Unknown/Assets/Scripts/Enemies Scripts/Spider/PurpleProjectile.cs
+++ Dark Unknown/Assets/Scripts/Enemies Scripts/Spider/PurpleProjectile.cs
@@ -1,22 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class PurpleProjectile : MonoBehaviour
 {
     [SerializeField] private float _damage = 2f;
+    private Animator _animator;
 
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        StartCoroutine(DeactivateCollider(0.3f));
+    }
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Collider2D[] hitEnemies = collision.GetComponentsInChildren<Collider2D>();
+        _animator.SetTrigger("destroy");
         foreach (Collider2D player in hitEnemies)
         {
             if (player.gameObject.CompareTag("Player"))
             {
                 Debug.Log("damage");
                 player.GetComponentInParent<Player>().TakeDamage(_damage);
-                Destroy(gameObject);
             }
         }
+
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+    }
+    
+    private IEnumerator DeactivateCollider(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        GetComponent<BoxCollider2D>().enabled = true;
+    }
+
+    public void Destroy()
+    {
+        Destroy(gameObject);
     }
 }
