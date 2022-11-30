3ea60b89945c5584d62c01d95319031ffe9c86cf
MattiaC <mattia.carini1998@gmail.com>
2022-11-30 20:00:59 +0100

    Fix initial room

--- Dark Unknown/Assets/Scripts/RoomElement/RoomLogic.cs
+++ Dark Unknown/Assets/Scripts/RoomElement/RoomLogic.cs
@@ -14,10 +14,9 @@ public class RoomLogic : MonoBehaviour
     private List<EnemyController> _enemies = new List<EnemyController>();
     private EnemySpawner _enemySpawner;
 
-    [Header("")]
+    [Header("Doors")]
     [SerializeField] private Door[] _doors;
-    [SerializeField] private Transform _spawnPointReward;
-    private Reward _rewardSpawned;
+    [SerializeField] private List<SymbolType> _possibleSymbols = new List<SymbolType>();
 
     [Header("Rewards")]
     [SerializeField] private HealthReward _healthReward;
@@ -25,6 +24,9 @@ public class RoomLogic : MonoBehaviour
     [SerializeField] private BowReward _bowReward;
     [SerializeField] private StrengthReward _strengthReward;
 
+    [SerializeField] private Transform _spawnPointReward;
+    private Reward _rewardSpawned;
+    
     public enum Type {INITIAL, RANDOM, HEALTH, BOW, SPEED, STRENGTH};
     private Type _roomType;
     private bool _isControllEnabled = true;
@@ -36,7 +38,14 @@ public class RoomLogic : MonoBehaviour
 
         //initialize _enemySpawner and call the coroutine which call the enemySpawner method to spawn all enemies 
         _enemySpawner = GetComponent<EnemySpawner>();
-        //_rewardSpawned = Instantiate(_rewardSpawned, _spawnPointReward.position, Quaternion.identity);
+        
+        //Set door symbols all different from each other
+        foreach (Door d in _doors)
+        {
+            int i = Random.Range(0, _possibleSymbols.Count);
+            d.setSymbol(_possibleSymbols[i]);
+            _possibleSymbols.RemoveAt(i);
+        }
     }
  
     // Update is called once per frame
