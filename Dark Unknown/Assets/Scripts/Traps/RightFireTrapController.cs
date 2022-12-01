f8c24b4a747d49d7be1edc0d7518a8d9c3e7e2aa
MattiaC <mattia.carini1998@gmail.com>
2022-12-01 21:48:02 +0100

    Populated rooms 8 and 9

---
+++ Dark Unknown/Assets/Scripts/Traps/RightFireTrapController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightFireTrapController : MonoBehaviour
+{
+    [SerializeField] private GameObject _fireball;
+    [SerializeField] private float _velocity = 6f;
+    [SerializeField] private Transform _spawnFireballPoint;
+    [SerializeField] private float _fireballSecondsEnabled = 0.02f;
+
+    void Fire()
+    {
+        GameObject fireball = Instantiate(_fireball, _spawnFireballPoint.position, Quaternion.identity);
+        fireball.GetComponent<Rigidbody2D>().velocity = Vector2.right * _velocity;
+
+        //Enable fireball after _fireballSecondsEnabled sec 
+        StartCoroutine(fireball.GetComponent<Fireball>().DeactivateCollider(_fireballSecondsEnabled));
+    }
+}
