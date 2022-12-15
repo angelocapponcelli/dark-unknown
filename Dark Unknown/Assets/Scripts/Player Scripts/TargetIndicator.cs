971f60d7d21708dec5b9777fc6ac150b670583a7
MatteoSanti-prog <69008453+MatteoSanti-prog@users.noreply.github.com>
2022-12-15 17:12:21 +0100

    Commit TargetIndicator

---
+++ Dark Unknown/Assets/Scripts/Player Scripts/TargetIndicator.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetIndicator : MonoBehaviour
+{
+    public Transform target;
+    [SerializeField] public float hideDistance;
+
+    void Update()
+    {
+        var dir = target.position - transform.position;
+
+        if (dir.magnitude < hideDistance)
+        {
+            SetChildrenActive(false);
+        }
+        else
+        {
+            SetChildrenActive(true);
+        
+            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+
+    void SetChildrenActive(bool value)
+    {
+        transform.gameObject.SetActive(value);
+    }
+}
