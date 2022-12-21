ed1079296de26153259581c8e9f8e4419e5a22ad
angelocapponcelli <angelo.capponcelli@mail.polimi.it>
2022-12-21 16:48:53 +0100

    Axe attack versions
    Two axe attack versions: based on distance or targetpoint

--- Dark Unknown/Assets/Scripts/Weapons Scripts/AxeAttack.cs
+++ Dark Unknown/Assets/Scripts/Weapons Scripts/AxeAttack.cs
@@ -2,26 +2,31 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AxeAttack : MonoBehaviour
 {
+    //ver1: axe launch based on the pointer / ver2: axe launch based on the distance
     [SerializeField] private float _damage = 10f;
     private bool _returnToPlayer = false;
     private Vector2 _target;
     private Rigidbody2D _rigidbody2D;
     private int _countTriggerStay = 0;
+    [SerializeField] private float _distance=0.45f; //ver2
 
     private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        StartCoroutine(timeToReturn(_distance)); //ver2
     }
 
     private void Update()
     {
         if (!_returnToPlayer)
         {
-           Vector2 newPosition = Vector2.MoveTowards(transform.position, _target, Time.deltaTime * 8);
+           /* ver1
+            Vector2 newPosition = Vector2.MoveTowards(transform.position, _target, Time.deltaTime * 8);
            if (transform.position == (Vector3) _target)
            {
                _returnToPlayer = true;
@@ -29,7 +34,7 @@ public class AxeAttack : MonoBehaviour
            else
            {
                _rigidbody2D.MovePosition(newPosition);
-           }
+           }*/
         }
         else
         {
@@ -61,7 +66,6 @@ public class AxeAttack : MonoBehaviour
         }
 
         if (collision.CompareTag("Trap")) return;
-        //Destroy(gameObject);
     }
     
     public void setTarget(Vector2 target)
@@ -83,5 +87,12 @@ public class AxeAttack : MonoBehaviour
                 Destroy(gameObject);
             }
         }
+    }
+    
+    //ver2
+    private IEnumerator timeToReturn(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        _returnToPlayer = true;
     }
 }
