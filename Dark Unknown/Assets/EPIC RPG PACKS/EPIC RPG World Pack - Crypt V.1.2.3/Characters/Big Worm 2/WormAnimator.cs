c741d9ca329f029e0a175cba6d9101588e6cd718
lucafornasari <62260158+lucafornasari@users.noreply.github.com>
2022-12-21 13:28:54 +0100

    Worm v1

---
+++ Dark Unknown/Assets/EPIC RPG PACKS/EPIC RPG World Pack - Crypt V.1.2.3/Characters/Big Worm 2/WormAnimator.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormAnimator : MonoBehaviour
+{
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    
+    public void AnimateEnemy(bool isMoving, Vector2 direction)
+    {
+        if (isMoving) flip(direction);
+    }
+
+    public void Show()
+    {
+        _animator.SetTrigger("Exit");
+    }
+
+    public void Hide()
+    {
+        _animator.SetTrigger("Hide");
+    }
+
+    public void Attack()
+    {
+        _animator.SetTrigger("Attack");
+    }
+    
+    public void flip(Vector2 direction)
+    {
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        if (Mathf.Abs(angle) < 90)
+        {
+            gameObject.transform.localScale = new Vector3(Mathf.Abs(gameObject.transform.localScale.x), gameObject.transform.localScale.y, gameObject.transform.localScale.z);
+        }
+        else
+        {
+            gameObject.transform.localScale = new Vector3(Mathf.Abs(gameObject.transform.localScale.x) * -1, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
+        }
+    }
+    
+}
