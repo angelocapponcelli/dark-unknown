99c1751dc19bde749d0c761de00e7700fe6f399b
MattiaC <mattia.carini1998@gmail.com>
2023-01-05 13:56:47 +0100

    Update Beam.cs

--- Dark Unknown/Assets/EPIC RPG PACKS/EPIC RPG World Pack - Sewers V1.3/Characters/Beholder/Beam.cs
+++ Dark Unknown/Assets/EPIC RPG PACKS/EPIC RPG World Pack - Sewers V1.3/Characters/Beholder/Beam.cs
@@ -6,10 +6,8 @@ public class Beam : MonoBehaviour
     public float laserBeamLength;
     private LineRenderer _lineRenderer;
     [SerializeField] private float rotationSpeed = 50f;
-    //private Transform _beamColliderPos;
     private BoxCollider2D _beamCollider;
     private Transform _impactPoint;
-    //private RaycastHit2D _hit;
 
     public Vector3 beginPos = new Vector3(0, 0, 0);
     public Vector3 endPos = new Vector3(0, 0, 0);
@@ -26,11 +24,7 @@ public class Beam : MonoBehaviour
     {
         _lineRenderer = GetComponent<LineRenderer>();
         _impactPoint = transform.GetChild(0).transform;
-        //_impactPoint = _lineRenderer.transform.GetChild(0).transform;
-        //_beamColliderPos = transform.GetChild(1).transform;
         _beamCollider = GetComponent<BoxCollider2D>();
-        //_beamCollider = GetComponentInChildren<BoxCollider2D>();
-        //beginPos = transform.position;
     }
 
     private void Start()
@@ -39,7 +33,6 @@ public class Beam : MonoBehaviour
         endPos = beginPos + transform.right * laserBeamLength; // transform.right is normalized vector 1 to right
         _lineRenderer.SetPositions(new Vector3[] {beginPos, endPos });
         _impactPoint.position = endPos;
-        //var temp = new Vector2(endPos.x, 0);
         _beamCollider.offset = new Vector2(endPos.x/2, 0);
         _beamCollider.size = new Vector2(endPos.x, 1);
     }
@@ -47,21 +40,12 @@ public class Beam : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        //Calculate new position 
-        /*//Vector3 newBeginPos = transform.localToWorldMatrix * new Vector4(beginPos.x, beginPos.y, beginPos.z, 1);
-        
-
-        //Apply new position
-        //_lineRenderer.SetPosition(0, newBeginPos);
-        _lineRenderer.SetPosition(1, newEndPos);*/
-        
         transform.Rotate(Vector3.forward * (rotationSpeed * Time.deltaTime));
         var hit = Physics2D.Raycast((Vector2)beginPos, 
             _lineRenderer.transform.right, laserBeamLength);
 
         if (hit)
         {
-            Debug.Log("hit");
             var mod = Mathf.Sqrt(hit.point.x * hit.point.x + hit.point.y * hit.point.y);
             endPos = new Vector3(mod, 0, 0);
         }
@@ -69,11 +53,8 @@ public class Beam : MonoBehaviour
         {
             endPos = beginPos + Vector3.right * laserBeamLength;
         }
-        //Vector3 newEndPos   = transform.localToWorldMatrix * new Vector4(endPos.x, endPos.y, endPos.z, 1);
         _lineRenderer.SetPosition(1, new Vector3(Mathf.Abs(endPos.x),0,0));
         _impactPoint.position = _impactPoint.position.normalized * endPos.x;
-        //Vector3.MoveTowards(_impactPoint.position,endPos,.1f);
-        //_impactPoint.position = new Vector3(endPos.x, Mathf.Cos(transform.rotation.z));
         _beamCollider.offset = new Vector2(Mathf.Abs(endPos.x)/2, 0);
         _beamCollider.size = new Vector2(Mathf.Abs(endPos.x), 1);
 
@@ -81,14 +62,12 @@ public class Beam : MonoBehaviour
         
         // Animates the beam
         _fpsCounter += Time.deltaTime;
-        if (_fpsCounter >= 1f / fps)
-        {
-            _animationStep++;
-            if (_animationStep == textures.Length) _animationStep = 0;
+        if (!(_fpsCounter >= 1f / fps)) return;
+        _animationStep++;
+        if (_animationStep == textures.Length) _animationStep = 0;
         
-            _lineRenderer.material.SetTexture("_MainTex", textures[_animationStep]);
+        _lineRenderer.material.SetTexture("_MainTex", textures[_animationStep]);
 
-            _fpsCounter = 0f;
-        }
+        _fpsCounter = 0f;
     }
 }
